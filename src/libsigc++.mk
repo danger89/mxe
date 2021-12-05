6033fce72706da38b6e657e05fc64346902f39f3
Melroy van den Berg <melroy@melroy.org>
2021-12-05 01:36:45 +0100

    Update libsigc++

--- src/libsigc++.mk
+++ src/libsigc++.mk
@@ -3,19 +3,26 @@
 PKG             := libsigc++
 $(PKG)_WEBSITE  := https://libsigc.sourceforge.io/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.9.3
-$(PKG)_CHECKSUM := 0bf9b301ad6198c550986c51150a646df198e8d1d235270c16486b0dda30097f
+$(PKG)_VERSION  := 3.0.7
+$(PKG)_CHECKSUM := bfbe91c0d094ea6bbc6cbd3909b7d98c6561eea8b6d9c0c25add906a6e83d733
 $(PKG)_GH_CONF  := libsigcplusplus/libsigcplusplus/releases,,,99,,.tar.xz
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_URL_2    := https://download.gnome.org/sources/libsigc++/$(call SHORT_PKG_VERSION,$(PKG))/libsigc++-$($(PKG)_VERSION).tar.xz
-$(PKG)_DEPS     := cc
+$(PKG)_DEPS     := cc meson-wrapper
 
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
-        $(MXE_CONFIGURE_OPTS) \
-        CXX='$(TARGET)-g++' \
-        PKG_CONFIG='$(PREFIX)/bin/$(TARGET)-pkg-config' \
-        MAKE=$(MAKE)
-    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT)
-    $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_CRUFT)
+    $(MXE_MESON_WRAPPER) --buildtype=release \
+        -Dbuild-documentation=false \
+        -Dbuild-examples=false \
+        '$(BUILD_DIR)' '$(SOURCE_DIR)' && \
+    ninja -C '$(BUILD_DIR)' -j '$(JOBS)' && \
+    ninja -C '$(BUILD_DIR)' -j '$(JOBS)' install
+
+    #cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
+    #    $(MXE_CONFIGURE_OPTS) \
+    #    CXX='$(TARGET)-g++' \
+    #    PKG_CONFIG='$(PREFIX)/bin/$(TARGET)-pkg-config' \
+    #    MAKE=$(MAKE)
+    #$(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT)
+    #$(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_CRUFT)
 endef
